235cd1af284847bd2085f1f71b92bcb500b1f204
= <=>
2020-01-07 15:00:56 +0530

    file upload committed

---
+++ src/app/CSVModel.ts
@@ -0,0 +1,11 @@
+export class CSVRecord {
+    public name: any;
+    public email: any;
+    public phoneNumber: any;
+    public sourceAccountCurrency: any;
+    public recipientAccountCurrency: any;
+    public amount: any;
+    public schedule: any;
+    public reference: any;
+
+}
---
+++ src/app/app.component.ts
@@ -0,0 +1,84 @@
+import { Component, ViewChild } from '@angular/core';
+import { CSVRecord } from './CSVModel';  
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  
+  title = 'fileCSV';  
+  
+  public records: any[] = [];  
+  @ViewChild ('csvReader') csvReader: any;  
+  
+  uploadListener($event: any): void {  
+  
+    let text = [];  
+    let files = $event.srcElement.files;  
+  
+    if (this.isValidCSVFile(files[0])) {  
+  
+      let input = $event.target;  
+      let reader = new FileReader();  
+      reader.readAsText(input.files[0]);  
+  
+      reader.onload = () => {  
+        let csvData = reader.result;  
+        let csvRecordsArray = (<string>csvData).split(/\r\n|\n/);  
+  
+        let headersRow = this.getHeaderArray(csvRecordsArray);  
+  
+        this.records = this.getDataRecordsArrayFromCSVFile(csvRecordsArray, headersRow.length);  
+      };  
+  
+      reader.onerror = function () {  
+        console.log('error is occured while reading file!');  
+      };  
+  
+    } else {  
+      alert("Please import valid .csv file.");  
+      this.fileReset();  
+    }  
+  }  
+  
+  getDataRecordsArrayFromCSVFile(csvRecordsArray: any, headerLength: any) {  
+    let csvArr = [];  
+  
+    for (let i = 1; i < csvRecordsArray.length; i++) {  
+      let curruntRecord = (<string>csvRecordsArray[i]).split(',');  
+      if (curruntRecord.length == headerLength) {  
+        let csvRecord: CSVRecord = new CSVRecord();  
+        csvRecord.name = curruntRecord[0].trim();  
+        csvRecord.email = curruntRecord[1].trim();  
+        csvRecord.phoneNumber = curruntRecord[2].trim();  
+        csvRecord.sourceAccountCurrency = curruntRecord[3].trim();  
+        csvRecord.recipientAccountCurrency = curruntRecord[4].trim();  
+        csvRecord.amount = curruntRecord[5].trim();  
+        csvRecord.schedule = curruntRecord[6].trim();
+        csvRecord.reference = curruntRecord[7].trim();
+        csvArr.push(csvRecord);  
+      }  
+    }  
+    return csvArr;  
+  }  
+  
+  isValidCSVFile(file: any) {  
+    return file.name.endsWith(".csv");  
+  }  
+  
+  getHeaderArray(csvRecordsArr: any) {  
+    let headers = (<string>csvRecordsArr[0]).split(',');  
+    let headerArray = [];  
+    for (let j = 0; j < headers.length; j++) {  
+      headerArray.push(headers[j]);  
+    }  
+    return headerArray;  
+  }  
+  
+  fileReset() {  
+    this.csvReader.nativeElement.value = "";  
+    this.records = [];  
+  }  
+}  
+
